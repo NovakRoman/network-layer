3424456aba074cd85206e60688dca761b22e2b06
NovakRoman <roman.ss.novak@gmail.com>
2024-04-05 20:17:58 +0300

    task-2  added, all updated

--- src/users.js
+++ src/users.js
@@ -6,11 +6,44 @@ UsersInformation.prototype.getAllUsers = async function () {
   if (response.ok) {
     const users = await response.json();
     console.log(users);
-    users.forEach(item => { console.log('id', item.id); })
-    users.forEach(item => { console.log('name', item.name); })
-    users.forEach(item => { console.log('email', item.email); })
-    users.forEach(item => { console.log('city', item.address.city); })
+    users.forEach(item => { console.log(`id (${item.id}), name (${item.name}), email (${item.email}), phone (${item.phone}), company name(${item.company.name})`); })
     
+    const groupedByCity = users.reduce((total, user) => {
+    return {
+    ...total,
+    [user.address.city]: [...(total[user.address.city] || []), user]
+    };
+    }, {} );
+    console.log(groupedByCity);
+
+    users.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
+    console.log(users);
+
+    const updateddUsers = [...users, ]
+    console.log(updateddUsers);
+
+
+    const newUsers = [];
+    function restUsers({username, email, address, website, company, ...rest}) {
+      return {...rest}
+    }
+
+    users.forEach(item => {
+      newUsers.push(
+        restUsers(item)
+      )
+    })
+    console.log(newUsers);
+
+
     return users;
   } else {
   alert("Ошибка HTTP: " + response.status);
